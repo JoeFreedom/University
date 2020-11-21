1b17239d12bc3fd723a6de29ae7201da97b27ef0
Дмитрий Саранцев <sarancev.vrn@gmail.com>
2020-11-21 19:15:54 +0300

    Add sql files

---
+++ SQL/table_froup.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `host1323541_university`.`table_group` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `education_form` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
---
+++ SQL/table_student.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `host1323541_university`.`table_student` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `status` INT NOT NULL,
+  `id_group` INT NOT NULL,
+  `id_contact` INT NOT NULL,
+  `payment_form` VARCHAR(45) NOT NULL,
+  `id_person` INT NOT NULL,
+  PRIMARY KEY (`id`));
