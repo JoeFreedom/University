b1ba06c5c6b2e6ec54c931c14af353e2e4f9a4f4
Joel Isuwa <joelgaiciyadu@gmail.com>
2020-11-25 13:07:27 +0300

    Recreated table_contacts to fix #10
    added column id_person

--- SQL/table_contacts.sql
+++ SQL/table_contacts.sql
@@ -1,10 +1,8 @@
-
 create table table_contacts
 (
-    id         int auto_increment
-        primary key,
-    id_student int not null,
+    id         int auto_increment primary key,
     id_address int not null,
     id_email   int not null,
-    id_phone   int not null
+    id_phone   int not null,
+    id_person  int not null
 );
