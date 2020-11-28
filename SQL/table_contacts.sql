e1342d6d3e965255d26f015491d146331da0a933
SergeySamodurovS <god-sergey@yandex.ru>
2020-11-28 19:05:31 +0300

    Update table_contacts.sql
    #20 table_contacts

--- SQL/table_contacts.sql
+++ SQL/table_contacts.sql
@@ -1,8 +1,18 @@
 create table table_contacts
 (
-    id         int auto_increment primary key,
-    id_address int not null,
-    id_email   int not null,
-    id_phone   int not null,
-    id_person  int not null
-);
+	id int auto_increment
+		primary key,
+	id_address int not null,
+	id_email int not null,
+	id_phone int not null,
+	id_person int not null,
+	constraint table_contacts_table_address_id_fk
+		foreign key (id_address) references table_address (id),
+	constraint table_contacts_table_email_id_fk
+		foreign key (id_email) references table_email (id),
+	constraint table_contacts_table_person_id_fk
+		foreign key (id_person) references table_person (id),
+	constraint table_contacts_table_phone_id_fk
+		foreign key (id_phone) references table_phone (id)
+)
+comment 'Таблица контактов';
