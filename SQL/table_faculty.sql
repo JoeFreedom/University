9b79d54cab6a6cd10cb83572629e48e877410e29
Lev <58858416+blbrxthne5@users.noreply.github.com>
2020-11-28 18:23:35 +0300

    Update table_faculty.sql

--- SQL/table_faculty.sql
+++ SQL/table_faculty.sql
@@ -3,8 +3,10 @@ create table table_faculty
     id           int auto_increment
         primary key,
     name         varchar(100) not null,
-    dekan        varchar(100) not null,
     id_specialty int          not null,
-    cathedra     varchar(100) not null
+    cathedra     varchar(100) not null,
+    constraint table_faculty_table_specialty_id_fk
+        foreign key (id_specialty) references table_specialty (id)
 );
+
 
