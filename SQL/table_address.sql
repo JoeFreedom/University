784f5a1a34eb22f186b87444ef21c64870a13afc
SergeySamodurovS <god-sergey@yandex.ru>
2020-11-28 17:15:32 +0300

    Update table_address.sql
    #19 renamed row and title of table_grant

--- SQL/table_address.sql
+++ SQL/table_address.sql
@@ -4,7 +4,7 @@ create table table_address
     country   varchar(30)  not null check (country !=''),
     region    varchar(50)  not null check (region !=''),
     street    varchar(100) not null check (street !=''),
-    house     varchar(10)  not null check (house !=''),
+    building  varchar(10)  not null check (house !=''),
     apartment smallint     not null,
     zip_code  varchar(10) not null check (zip_code !=''),
     address_Type varchar(50) not null check ( address_Type !='')
