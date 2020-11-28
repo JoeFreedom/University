038f55014f1d50b656ac7bb34da8564b95b7667d
SergeySamodurovS <god-sergey@yandex.ru>
2020-11-28 17:20:38 +0300

    Update table_subject.sql
    #19 renamed row and title of table_subject

--- SQL/table_subject.sql
+++ SQL/table_subject.sql
@@ -2,7 +2,7 @@ create table table_subject
 (
 	id int auto_increment,
 	name nvarchar(100) not null check (name <> N''),
-	number_of_hours int not null check (number_of_hours > 0),
+	hours_of_study int not null check (number_of_hours > 0),
 	session int not null check (session >= 1),
 	constraint table_subject_pk
 		primary key (id)
