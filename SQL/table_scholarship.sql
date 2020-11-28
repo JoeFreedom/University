852270e98d84a768e7186d5e022674d5f35cada4
Lev <58858416+blbrxthne5@users.noreply.github.com>
2020-11-28 18:29:23 +0300

    Update table_scholarship.sql

--- SQL/table_scholarship.sql
+++ SQL/table_scholarship.sql
@@ -1,10 +1,13 @@
 
 create table table_scholarship
 (
-    id         int auto_increment primary key,
-    scholarship_type varchar(50) not null,
-    id_student int not null,
-    start_date date not null,
-    end_date date not null,
-    scholarship_amount double not null default 0.00
+    id               int auto_increment
+        primary key,
+    scholarship_type varchar(50)      not null,
+    id_student       int              not null,
+    start_date       date             not null,
+    end_date         date             not null,
+    grand_Amount     double default 0 not null,
+    constraint table_grant_table_student_id_fk
+        foreign key (id_student) references table_student (id)
 );
