13abd4fb599704f494b74ec248586448f6a5e86c
XMelexus <58857939+XMelexus@users.noreply.github.com>
2020-11-28 18:01:53 +0300

    Синхронизация с реальностью

--- SQL/table_student.sql
+++ SQL/table_student.sql
@@ -1,8 +1,16 @@
-CREATE TABLE `host1323541_university`.`table_student` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `status` INT NOT NULL,
-  `id_group` INT NOT NULL,
-  `id_contact` INT NOT NULL,
-  `payment_form` VARCHAR(45) NOT NULL,
-  `id_person` INT NOT NULL,
-  PRIMARY KEY (`id`));
+create table table_student
+(
+    id           int         not null
+        primary key,
+    status       int         not null,
+    id_group     int         not null,
+    id_contact   int         not null,
+    payment_form varchar(45) not null,
+    id_person    int         not null,
+    constraint table_student_table_contacts_id_fk
+        foreign key (id_contact) references table_contacts (id),
+    constraint table_student_table_group_id_fk
+        foreign key (id_group) references table_group (id),
+    constraint table_student_table_person_ID_fk
+        foreign key (id_person) references table_person (id)
+);
