652da2cd85d55f6781e85c20830b055e9d11872d
XMelexus <58857939+XMelexus@users.noreply.github.com>
2020-11-29 16:23:00 +0300

    Update table_student.sql

--- SQL/table_student.sql
+++ SQL/table_student.sql
@@ -1,16 +1,23 @@
+-- auto-generated definition
 create table table_student
 (
-    id           int         not null
-        primary key,
-    status       int         not null,
+    id           int auto_increment,
+    status       varchar(20) not null,
     id_group     int         not null,
     id_contact   int         not null,
     payment_form varchar(45) not null,
     id_person    int         not null,
+    constraint table_student_id_uindex
+        unique (id),
     constraint table_student_table_contacts_id_fk
         foreign key (id_contact) references table_contacts (id),
-    constraint table_student_table_group_id_fk
-        foreign key (id_group) references table_group (id),
-    constraint table_student_table_person_ID_fk
+    constraint table_student_table_person_id_fk
         foreign key (id_person) references table_person (id)
 );
+
+create index table_student_table_group_id_fk
+    on table_student (id_group);
+
+alter table table_student
+    add primary key (id);
+
