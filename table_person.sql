0d8f9a9235ef62ef690cf078cbf36d03d4ae1ee0
Joel Isuwa <joelgaiciyadu@gmail.com>
2020-11-30 10:43:42 +0300

    Delete table_person.sql
    Didn't create in the folder expected.

--- table_person.sql
+++
@@ -1,8 +0,0 @@
-create table table_person
-(
-	id int auto_increment primary key,
-	first_name varchar(64) not null,
-	last_name varchar(64) not null,
-	middle_name varchar(64) not null,
-	sex char not null
-)
